f1c8fa9f38bbd4fad59d47a008f5175b28591212
Hugo Ferreira <hugo@ferreira.cc>
2018-02-28 04:14:59 +0000

    Install key addons for kops

---
+++ kube/install-addons.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo
+echo "### Installing Heapster (provides basic cluster monitoring) ###"
+echo
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/monitoring-standalone/v1.7.0.yaml
+
+echo
+echo "### Installing Kubernetes Dashboard ###"
+echo
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.7.1.yaml
